ef2f3865f2123ffbd0401591676697d6cef8c285
Ryan <ryanshahine@msn.com>
2021-10-25 13:20:24 +0200

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,50 @@ import Graphics.Gloss.Interface.IO.Game
 
 main :: IO ()
 main = do
+      wall0 <- loadBMP "sprites/left.bmp"
+      wall1 <- loadBMP "sprites/right.bmp"
+      wall2 <- loadBMP "sprites/bottom.bmp"
+      wall3 <- loadBMP "sprites/top.bmp"
+
+      wall4 <- loadBMP "sprites/bottomleftcorner.bmp"
+      wall5 <- loadBMP "sprites/bottomrightcorner.bmp"
+      wall6 <- loadBMP "sprites/topleftcorner.bmp"
+      wall7 <- loadBMP "sprites/toprightcorner.bmp"
+
+      wall8 <- loadBMP "sprites/doubleleft.bmp"
+      wall9 <- loadBMP "sprites/doubleright.bmp"
+      wall10 <- loadBMP "sprites/doublebottom.bmp"
+      wall11 <- loadBMP "sprites/doubletop.bmp"
+
+      wall12 <- loadBMP "sprites/doubletopleft.bmp"
+      wall13 <- loadBMP "sprites/doubletopright.bmp"
+      wall14 <- loadBMP "sprites/doublebottomleft.bmp"
+      wall15 <- loadBMP "sprites/doublebottomright.bmp"
+
+      wall16 <- loadBMP "sprites/doubleinleft.bmp"
+      wall17 <- loadBMP "sprites/doubleinright.bmp"
+      wall18 <- loadBMP "sprites/doubleinupleft.bmp"
+      wall19 <- loadBMP "sprites/doubleinupright.bmp"
+
+      wall20 <- loadBMP "sprites/doublesquaretopleft.bmp"
+      wall21 <- loadBMP "sprites/doublesquaretopright.bmp"
+      wall22 <- loadBMP "sprites/doublesquarebottomleft.bmp"
+      wall23 <- loadBMP "sprites/doublesquarebottomright.bmp"
+
+      wall24 <- loadBMP "sprites/doublesquareleft.bmp"
+      wall25 <- loadBMP "sprites/doublesquareright.bmp"
+
+      blank <- loadBMP "sprites/blank.bmp"
+      dotpiece <- loadBMP "sprites/dotpiece.bmp"
+
+
       playerImg <- loadBMP "assets/pacmanleft1.bmp"
-      wallImg <- loadBMP "assets/bottom.bmp"
 
       play 
         (InWindow "Pac-Man" (448, 576) (0, 0))
         red                   -- Background color
         10                    -- Frames per second
         initialState          -- Initial state
-        (`render` [wallImg, playerImg])  -- View function
+        (`render` [wall0, wall1, wall2, wall3, wall4, wall5, wall6, wall7, wall8, wall9, wall10, wall11, wall12, wall13, wall14, wall15, wall16, wall17, wall18, wall19, wall20, wall21, wall22, wall23, wall24, wall25, blank, dotpiece, playerImg])  -- View function
         input                 -- Event function
         step                  -- Step function
--- app/View.hs
+++ app/View.hs
@@ -5,8 +5,38 @@ import Model
 
 renderBoardItem :: BoardItem -> Float -> Float -> [Picture] -> Picture
 renderBoardItem boardItem xIndex yIndex images
-  | boardItem == Wall 1 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 224) (head images)
-  | boardItem == Floor = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 224) (rectangleSolid 16 16)
+  | boardItem == Wall 0 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 0)
+  | boardItem == Wall 1 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 1)
+  | boardItem == Wall 2 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 2)
+  | boardItem == Wall 3 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 3)
+  | boardItem == Wall 4 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 4)
+  | boardItem == Wall 5 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 5)
+  | boardItem == Wall 6 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 6)
+  | boardItem == Wall 7 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 7)
+  | boardItem == Wall 8 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 8)
+  | boardItem == Wall 9 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 9)
+  | boardItem == Wall 10 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 10)
+  | boardItem == Wall 11 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 11)
+  | boardItem == Wall 12 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 12)
+  | boardItem == Wall 13 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 13)
+  | boardItem == Wall 14 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 14)
+  | boardItem == Wall 15 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 15)
+  | boardItem == Wall 16 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 16)
+  | boardItem == Wall 17 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 17)
+  | boardItem == Wall 18 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 18)
+  | boardItem == Wall 19 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 19)
+  | boardItem == Wall 20 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 20)
+  | boardItem == Wall 21 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 21)
+  | boardItem == Wall 22 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 22)
+  | boardItem == Wall 23 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 23)
+  | boardItem == Wall 24 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 24)
+  | boardItem == Wall 25 = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 25)
+
+
+  | boardItem == Floor = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 26)
+  | boardItem == Pellet NormalPellet = Translate ((xIndex * 16) - 224 + 8) ((yIndex * (-16)) + 295) (images !! 27)
+
+
   | boardItem == TeleportBarrier = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
   | boardItem == Gate = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
   | boardItem == Pellet NormalPellet = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
@@ -19,6 +49,7 @@ renderBoardItem boardItem xIndex yIndex images
   | boardItem == Fruit GalaxianFlagship 700 = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
   | boardItem == Fruit Bell 800 = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
   | boardItem == Fruit Key 900 = Translate (xIndex * 50) (yIndex * (-50)) (Circle 20.0)
+  | otherwise = error "exhaustive"
 
 renderRows :: Float -> Float -> Row -> [Picture] -> [Picture]
 renderRows _ _ [] _ = []
